07f68a905542fc312e481bb9f20e830a83a1dcb2
youta <youtatatsuke@gmail.com>
2024-02-11 21:22:32 +0900

    コメント等を消し忘れてたので、消して再度プッシュ。

--- RepsandSets.py
+++ RepsandSets.py
@@ -1,8 +1,6 @@
-#randomで生成
 import random
 import tkinter as tk
-#list[random.randint(1,7)]
-#print(list[0])
+
 def logic():
     print("バーベルなら1、ダンベルなら２、マシンなら3を入力してください")
     trainingEquipment = int(input())
@@ -23,22 +21,4 @@ def logic():
     print('回数：' + str(number[0]))
     print('セット数' + str(number[1]))
 
-"""
-root = tk.Tk()
-root.title("トレーニング回数ルーレット")
-root.geometry("400x400")
-
-# ラベル
-lbl = tk.Label(text='トレーニング器具 ※ バーベル or ダンベル or マシン')
-lbl.place(x=30, y=70)
-
-# テキストボックス、入力
-txt = tk.Entry(width=20)
-txt.place(x=90, y=70)
-
-#テキストボックスの値が入力されたら回数とセット数を表示
-
-#テキストボックスの値を消す
-
-root.mainloop()
-"""
+logic()
